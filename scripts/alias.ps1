978f72f9f005d9403e590a8307688364934761be
Pedro Reys <pedroreys@gmail.com>
2011-09-08 13:33:52 -0500

    adding alias for notepad++

--- scripts/alias.ps1
+++ scripts/alias.ps1
@@ -1,2 +1,4 @@
 #creates an alias for hgtk. TortoiseHg 2.0 switched from hgtk to thg and sometimes it's hard to fix muscle memory
 Set-Alias hgtk thg
+
+Set-Alias note $ENV:PROGRAMFILES\Notepad++\notepad++.exe
